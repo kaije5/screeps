197032da02d640b17570c2fd0ee158e95e03b118
Kaije Henstra <53300974+kaije5@users.noreply.github.com>
2023-03-14 17:30:48 +0000

    add Visualize

--- src/functions/Dispatcher.ts
+++ src/functions/Dispatcher.ts
@@ -66,7 +66,7 @@ export function dispatch(status: number, creep: Creep) {
                 // change status to work status
                 creep.memory.status = 4;
             } else {
-                creep.moveTo(target.pos.x, target.pos.y);
+                creep.moveTo(target.pos.x, target.pos.y, { visualizePathStyle: { stroke: "#ffaa00" } });
             }
         }
     }
